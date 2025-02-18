b9c451e500ae3121006bc51e34f3f768c8093f09
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2025-02-18 20:59:56 -0300

    Added a back button behaviour

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { getContact, updateContact } from "../data";
 
@@ -22,6 +22,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 };
 
 export default function EditContact() {
+  const navigate = useNavigate();
   const { contact } = useLoaderData<typeof loader>();
 
   return (
@@ -68,7 +69,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => navigate(-1)}>
+          Cancel
+        </button>
       </p>
     </Form>
   );
